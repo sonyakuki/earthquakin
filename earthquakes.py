ec8c235b6cf3a4c15de543159d71ae0e17ee3b66
SonyaKuki <syk2123@columbia.edu>
2014-06-09 13:04:08 -0400

    Learned a little bit about functions

--- earthquakes.py
+++ earthquakes.py
@@ -1,1 +1,17 @@
-print "hello world" 
+
+def greeting (): 
+	name = raw_input("What is your name?")
+	print "Hello, " + name
+
+def hello(name):
+	print "Hello, " + name 
+
+def add(a, b):
+	return a + b
+
+hello("Sonya")
+
+add (2, 3)
+print add (2, 3) + add (4, 5)
+
+"A " + size_of_eq(earthquake_data) +" earthquake of 4.6 magnitude happened near California"
